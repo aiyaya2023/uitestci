09f8c7b4ffdb953bb385e00fb689f0e5340e4e90
aiyaya2023 <133057442+aiyaya2023@users.noreply.github.com>
2023-06-01 21:14:14 -0700

    Update cypress.config.js
    ddd

--- testcode/browser/cypress.config.js
+++ testcode/browser/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "ymhxfj",
+  projectId: 'ymhxfj',
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {
